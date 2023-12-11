af8885581504e70dc809ab51e7c4f3b08af44032
Ifanfomin <ifanfomin@rambler.ru>
2023-12-11 19:13:33 +0300

    Удалил лишние файлы

--- main.py
+++
@@ -1,6 +0,0 @@
-from classes_main import *
-from config import vk_token, host, user, password, database
-
-if __name__ == "__main__":
-    SaveGroupUsers("id_group", vk_token, host, user, password, database)
-    ShowDataBaseTables(host, user, password, database)
